3aac91f3a18c8d774a4188e5d3ae636d6df96d49
BeCon <selig2014@yandex.ru>
2025-06-10 12:13:30 +0900

    Added previous rendering clear before a new rendering

--- device/src/user_interface/widget_manager/vbox_layout.rs
+++ device/src/user_interface/widget_manager/vbox_layout.rs
@@ -4,35 +4,38 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 pub struct VBoxLayout {
-    pub x: u16,
-    pub y: u16,
-    pub spacing: u16,
+    cur_box: widget::Box<u16>,
+    prev_box: widget::Box<u16>,
+    spacing: u16,
     pub children: Vec<Rc<RefCell<dyn widget::Widget>>>,
-    current_y: u16,
-    width: u16,
 }
 
 impl widget::Widget for VBoxLayout {
-    fn render(&mut self) {
+    fn raw_render(&mut self) {
+        self.prev_box = self.cur_box;
         for child in &self.children {
-            child.borrow_mut().render();
+            child.borrow_mut().raw_render();
         }
     }
 
     fn set_position(&mut self, x: u16, y: u16) {
-        self.x = x;
-        self.y = y;
+        self.cur_box.x = x;
+        self.cur_box.y = y;
     }
 
     #[allow(unused)]
     fn set_size(&mut self, width: u16, height: u16) {}
 
     fn width(&self) -> u16 {
-        self.width
+        self.cur_box.width
     }
 
     fn height(&self) -> u16 {
-        self.current_y
+        self.cur_box.height
+    }
+
+    fn prev_geometry(&self) -> widget::Box<u16> {
+        self.prev_box
     }
 }
 
@@ -45,13 +48,20 @@ impl Layout for VBoxLayout {
 
 impl VBoxLayout {
     fn new(x: u16, y: u16) -> Self {
-        Self {
+        let cur_box = widget::Box::<u16> {
             x,
             y,
+            width: 0,
+            height: 0,
+        };
+
+        let prev_box = cur_box;
+
+        Self {
+            cur_box,
+            prev_box,
             spacing: 0,
             children: Vec::new(),
-            current_y: 0,
-            width: 0,
         }
     }
 
@@ -62,8 +72,8 @@ impl VBoxLayout {
 
     pub fn reflow(&mut self) {
         let children = self.children.clone();
-        self.current_y = 0;
-        self.width = 0;
+        self.cur_box.height = 0;
+        self.cur_box.width = 0;
         for child in &children {
             self.set_widget_position(child);
         }
@@ -71,8 +81,8 @@ impl VBoxLayout {
 
     fn set_widget_position(&mut self, widget: &Rc<RefCell<dyn widget::Widget>>) {
         let mut w = widget.borrow_mut();
-        w.set_position(self.x, self.y + self.current_y);
-        self.current_y += self.spacing + w.height();
-        self.width = self.width.max(w.width());
+        w.set_position(self.cur_box.x, self.cur_box.y + self.cur_box.height);
+        self.cur_box.height += self.spacing + w.height();
+        self.cur_box.width = self.cur_box.width.max(w.width());
     }
 }
--- device/src/user_interface/widget_manager/widget.rs
+++ device/src/user_interface/widget_manager/widget.rs
@@ -1,7 +1,40 @@
+use crate::user_interface::output::draw;
+
 pub trait Widget {
-    fn render(&mut self);
+    fn render(&mut self) {
+        self.clear();
+        self.raw_render();
+    }
+
+    fn clear(&mut self) {
+        let prev_box = self.prev_geometry();
+        draw::clear_box(prev_box.x, prev_box.y, prev_box.width, prev_box.height);
+    }
+
+    fn raw_render(&mut self);
+
     fn set_position(&mut self, x: u16, y: u16);
     fn set_size(&mut self, width: u16, height: u16);
     fn width(&self) -> u16;
     fn height(&self) -> u16;
+    fn prev_geometry(&self) -> Box<u16>;
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct Box<T> {
+    pub x: T,
+    pub y: T,
+    pub width: T,
+    pub height: T,
+}
+
+impl<T: Default> Default for Box<T> {
+    fn default() -> Self {
+        Self {
+            x: T::default(),
+            y: T::default(),
+            width: T::default(),
+            height: T::default(),
+        }
+    }
 }
